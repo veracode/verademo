290121e33fa781c36fb67d5e45b34e249fda1f01
Boy Baukema <bbaukema@veracode.com>
2017-11-08 12:45:20 +0100

    Add demo for CWE-134 Use of Externally Controlled Format String

--- src/main/java/com/veracode/verademo/controller/UserController.java
+++ src/main/java/com/veracode/verademo/controller/UserController.java
@@ -224,6 +224,48 @@ public class UserController {
 		logger.info("Redirecting to view: " + nextView);
 		return nextView;
 	}
+	
+	@RequestMapping(value = "/password-hint", method = RequestMethod.GET)
+	@ResponseBody
+	public String showPasswordHint(String username)
+	{
+		logger.info("Entering password-hint with username: " + username);
+		
+		if (username == null || username.isEmpty()) {
+			return "No username provided, please type in your username first";
+		}
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			Connection connect = DriverManager.getConnection(Constants.create().getJdbcConnectionString());
+	
+			String sql = "SELECT password FROM users WHERE username = '" + username + "'";
+			logger.info(sql);
+			Statement statement = connect.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			if (result.first()) {
+				String password= result.getString("password");
+				String formatString = "Username '" + username + "' has password: %.2s%s";
+				logger.info(formatString);
+				return String.format(
+						formatString,
+						password, 
+						String.format("%0" + (password.length() - 2) + "d", 0).replace("0", "*")
+				);
+			}
+			else {
+				return "No password found for " + username;
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "ERROR!";
+	}
 
 	@RequestMapping(value = "/logout", method = { RequestMethod.GET, RequestMethod.POST })
 	public String processLogout(
