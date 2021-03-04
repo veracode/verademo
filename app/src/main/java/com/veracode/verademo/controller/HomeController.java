c402c63f2c9be75c0bd1b21717dfd70fae591e21
Anthony Fielding <afielding@veracode.com>
2021-03-04 16:20:25 +0000

    Repair home controller

--- app/src/main/java/com/veracode/verademo/controller/HomeController.java
+++ app/src/main/java/com/veracode/verademo/controller/HomeController.java
@@ -3,12 +3,26 @@ package com.veracode.verademo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.ui.Model;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 @Controller
 public class HomeController {
+	private static final Logger logger = LogManager.getLogger("VeraDemo:HomeController");
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String getHome()
+	public String renderGet(Model model, HttpServletRequest request)
 	{
-		return "redirect:login";
+		// Check if user is already logged in
+		if (request.getSession().getAttribute("username") != null) {
+			// default to user's feed
+			return "redirect:feed";
+		}
+
+		model.addAttribute("javax.servlet.forward.request_uri", "/login");
+		model.addAttribute("username", "");
+		return "login";
 	}
 }
