c4de48a002c7592db08359e5bc8b5ac45bd0eb84
Anthony Fielding <afielding@veracode.com>
2021-03-25 16:37:54 +0000

    Various improvements

--- app/src/main/java/com/veracode/verademo/utils/Utils.java
+++ app/src/main/java/com/veracode/verademo/utils/Utils.java
@@ -9,29 +9,31 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
 
 public class Utils {
-	private static String getApplicationUrl() {
-		return System.getenv("APPLICATION_URL");
+	private static boolean isConnectionFromHttps() {
+		return System.getenv("CONNECTION_FROM_HTTPS") != null && System.getenv("CONNECTION_FROM_HTTPS").equals("1");
+	}
+
+	private static String getVirtualHost() {
+		return System.getenv("VIRTUAL_HOST");
 	}
 
 	public static String getSessionUserName(HttpServletRequest request, HttpServletResponse response) {
 		String username = (String) request.getSession().getAttribute("username");
-
-		if (getApplicationUrl() != null) {
-			upgradeCookieSecurityForHttps(response);
-		}
+		upgradeCookieSecurityForHttpsIfRequired(response);
 		
 		return username;
 	}
 
 	public static void setSessionUserName(HttpServletRequest request, HttpServletResponse response, String value) {
 		request.getSession().setAttribute("username", value);
-
-		if (getApplicationUrl() != null) {
-			upgradeCookieSecurityForHttps(response);
-		}
+		upgradeCookieSecurityForHttpsIfRequired(response);
 	}
 
-	private static void upgradeCookieSecurityForHttps(HttpServletResponse response) {
+	private static void upgradeCookieSecurityForHttpsIfRequired(HttpServletResponse response) {
+		if (!isConnectionFromHttps()) {
+			return;
+		}
+
 		Collection<String> headers = response.getHeaders(HttpHeaders.SET_COOKIE);
 		boolean firstHeader = true;
 		for (String header : headers) {
@@ -51,15 +53,12 @@ public class Utils {
 
 	public static void setCookie(HttpServletResponse response, String name, String value) {
 		response.addCookie(new Cookie(name, value));
-
-		if (getApplicationUrl() != null) {
-			upgradeCookieSecurityForHttps(response);
-		}
+		upgradeCookieSecurityForHttpsIfRequired(response);
 	}
 
 	public static String redirect(String destination) {
-		if (getApplicationUrl() != null) {
-			return "redirect:" + getApplicationUrl() + "/" + destination;
+		if (isConnectionFromHttps()) {
+			return "redirect:https://" + getVirtualHost() + "/" + destination;
 		}
 
 		return "redirect:" + destination;
